f1360081dd91bf486c7a005f8cd7464002a3a671
Your Name <kent.thomas@Kents-MacBook-Air.local>
2020-11-15 21:24:51 -0500

    reading in the data

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,22 @@
-var usga_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+// Creating a variable to append a map to the mapID div in the index.html
+var earthquakeMap = L.map("mapid", {
+    center: [37.7749, -122.4194],
+    zoom: 5
+  });
 
+// Grabing the MapBox API 
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(earthquakeMap);
 
+// API link for the USGA (All Earthquakes within the Week)
+var usga_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Calling the URL 
+d3.json(usga_url, function(data){
+    console.log(data)
+});
+    

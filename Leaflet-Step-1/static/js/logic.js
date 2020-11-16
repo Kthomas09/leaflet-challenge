907f9a0d7f4abe2fbec820c3abd877e116df7cb1
Your Name <kent.thomas@Kents-MacBook-Air.local>
2020-11-15 21:27:33 -0500

    the getColor's switch function is syntactically correct. Have to create the function to call into these fuctions to see if they are operational.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,7 +2,7 @@
 var earthquakeMap = L.map("mapid", {
     center: [37.7749, -122.4194],
     zoom: 5
-  });
+});
 
 // Grabing the MapBox API 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -16,18 +16,31 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var usga_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Calling the URL 
-d3.json(usga_url, function(data){
+d3.json(usga_url, function (data) {
     console.log(data)
-    function infoStyle (feature) {
+    function infoStyle(feature) {
         return {
             Opacity: 1,
             fillOpacity: 1,
             fillColor: getColor(feature.properties.mag),
             color: "#000000",
             radius: getRadius(features.properties.mag),
-            stroke: true, 
+            stroke: true,
             weight: 0.5
         };
-      }
+    }
+    function getColor(mag) {
+        switch (true) {
+            case mag > 5:
+                return "#FC0000";
+            case mag > 4:
+                return "#FF8000";
+            case mag > 3:
+                return "#FFFF00";
+            case mag > 2:
+                return "#78FF00";
+            case mag > 1:
+                return "#00FFFB";
+        }
+    }
 });
-    

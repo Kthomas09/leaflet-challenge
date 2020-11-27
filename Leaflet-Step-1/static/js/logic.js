10ceff252811601db829777a1c2b090344f23678
Your Name <kent.thomas@Kents-MacBook-Air.local>
2020-11-27 15:48:10 -0500

    reading in MapBox API with Access Token

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,3 +5,14 @@ var earthquakeMap = L.map("map_id", {
   center: [39.2890, -77.8597],
   zoom: 5
 });
+
+ // Adding a tile layer (the background map image) to the map
+  // Use the addTo method to add objects to the earthquakeMap
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(earthquakeMap);
